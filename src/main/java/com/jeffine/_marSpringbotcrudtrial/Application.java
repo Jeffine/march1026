cdeae7626c37e74c644c7b462a684c78f73d31d3
jeffine <benejeffine@gmail.com>
2026-04-06 23:05:32 +0530

    second commit

--- src/main/java/com/jeffine/_marSpringbotcrudtrial/Application.java
+++ src/main/java/com/jeffine/_marSpringbotcrudtrial/Application.java
@@ -3,7 +3,7 @@ package com.jeffine._marSpringbotcrudtrial;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication//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
 public class Application {
 
 	public static void main(String[] args) {
